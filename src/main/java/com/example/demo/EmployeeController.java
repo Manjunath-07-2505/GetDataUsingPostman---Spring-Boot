2c6bb1f9b025492f37b8a60935f0aa15e5443df8
Manjunath-07-2505 <manja.r2505@gmail.com>
2026-02-14 11:40:54 +0530

    Initial project upload

---
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+	@Autowired
+	EmployeeService employeeService;
+
+	@GetMapping("/employee/{id}")
+	public Employee getEmployeeId(@PathVariable int id) {
+		return employeeService.findEmployeeById(id);
+	}
+}
